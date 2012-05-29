1f88439208d30ce877ba766e88b836cf84ecd368
Ian Ownbey <ian@twitter.com>
2012-05-29 12:52:09 -0700

    Fuck

---
+++ picker.rb
@@ -0,0 +1,98 @@
+gifs = ["http://i.minus.com/ibzznoCimwYGmF.gif",
+"http://24.media.tumblr.com/tumblr_m4aru5zkNu1rrkiepo1_500.gif",
+"http://24.media.tumblr.com/tumblr_m4jni9MnFA1qzxzcso1_500.gif",
+"http://24.media.tumblr.com/tumblr_m4egjskuj81r5zq6ao1_r2_500.gif",
+"http://25.media.tumblr.com/tumblr_m4gr0yZkHF1r65mfio1_500.gif",
+"http://25.media.tumblr.com/tumblr_m28bcdWKpz1qkl2pjo1_500.gif",
+"http://24.media.tumblr.com/tumblr_m0lcad4ybK1rpx9bqo1_500.gif",
+"http://24.media.tumblr.com/tumblr_lyuxjfX9Dj1qc1jg4o2_500.gif",
+"http://25.media.tumblr.com/tumblr_m3k9abLnjC1rojkijo1_500.gif",
+"http://24.media.tumblr.com/tumblr_m3k8ig1Wbu1qg39ewo1_500.gif",
+"http://24.media.tumblr.com/tumblr_m26v9s5B4M1rto9yzo1_500.gif",
+"http://24.media.tumblr.com/tumblr_m4fmrq5sx81qz9aeqo1_400.gif",
+"http://24.media.tumblr.com/tumblr_m3h5frDSC31qk9s9mo1_500.gif",
+"http://24.media.tumblr.com/tumblr_m3seikcwKL1qi0kfro1_500.gif",
+"http://25.media.tumblr.com/tumblr_m4amucvWcl1qzpy7ko1_r1_500.gif",
+"http://25.media.tumblr.com/tumblr_m436196rZW1qz4u07o1_500.gif",
+"http://24.media.tumblr.com/tumblr_m3rxz3yYOo1r69ijxo1_500.gif",
+"http://24.media.tumblr.com/tumblr_m4cdr3MIwK1qfqz7no1_500.gif",
+"http://25.media.tumblr.com/tumblr_m3j7xadlEZ1qjvsoxo1_500.gif",
+"http://24.media.tumblr.com/tumblr_m3tt9j3yyL1rvwq31o1_500.gif",
+"http://25.media.tumblr.com/tumblr_m43o2iQeQo1qac1pao1_250.gif",
+"http://24.media.tumblr.com/tumblr_m42it7hMzK1qzgjfco1_500.gif",
+"http://25.media.tumblr.com/tumblr_m3zn6oLb181qg64q8o1_400.gif",
+"http://25.media.tumblr.com/tumblr_m3y15qqT6K1qe3p9bo1_500.gif",
+"http://25.media.tumblr.com/tumblr_m3xbkl0pxv1qeyjtwo1_500.gif",
+"http://24.media.tumblr.com/tumblr_m37uyuAMls1qap9uuo1_500.gif",
+"http://25.media.tumblr.com/tumblr_m3kfczYpdD1qjrlw4o1_500.gif",
+"http://25.media.tumblr.com/tumblr_m3j9b02NNT1qedb29o1_500.gif",
+"http://25.media.tumblr.com/tumblr_m3i909Sc5g1qzr9qko1_500.gif",
+"http://25.media.tumblr.com/tumblr_m3dhw55NKk1rtgsido1_500.gif",
+"http://25.media.tumblr.com/tumblr_m3geais5o31qcim6yo1_500.gif",
+"http://24.media.tumblr.com/tumblr_m3jlzyjfd91r8p3jmo1_500.gif",
+"http://25.media.tumblr.com/tumblr_m3oxvu22Lw1r0ix14o4_250.gif",
+"http://24.media.tumblr.com/tumblr_m41opfGeim1qap9uuo1_500.gif",
+"http://25.media.tumblr.com/tumblr_m48cch7Ohy1qaqnguo1_500.gif",
+"http://25.media.tumblr.com/tumblr_m1z174f1CW1qa1xnko1_500.gif",
+"http://24.media.tumblr.com/tumblr_m3cm0orPBF1qa9siqo1_500.gif",
+"http://25.media.tumblr.com/tumblr_lt59x8H9UJ1qm1s5co2_500.gif",
+"http://24.media.tumblr.com/tumblr_m341xhSkai1r207kmo1_r1_500.gif",
+"http://25.media.tumblr.com/tumblr_m2kvomtNgy1rslhj4o1_500.gif",
+"http://25.media.tumblr.com/tumblr_m2nfxvd5EK1qaaf7to1_500.gif",
+"http://25.media.tumblr.com/tumblr_m408vtndgV1r2ni29o1_500.gif",
+"http://25.media.tumblr.com/tumblr_m44vm2aBmg1r4fycuo1_r1_400.gif"]
+header =<<-EOD
+<html>
+  <head>
+    <style type="text/css">
+      .gifHolder {
+        height: 200px;
+        width: 200px;
+        border: 5px solid #AAB0BE;
+        float: left;
+        margin-left: 20px;
+        margin-bottom: 20px;
+        background-size: 100%;
+        overflow: hidden;
+      }
+      .gifChosen {
+        border-color: red;
+      }
+    </style>
+    <script>
+      currentGif = false
+      function setGif(gifId) {
+        gifDiv = document.getElementById(gifId)
+        if(currentGif) {
+          currentGif.className = "gifHolder"
+        }
+        gifUrl = gifDiv.getAttribute('data-gif-url')
+        gifDiv.className = "gifHolder gifChosen"
+        currentGif = gifDiv
+        document.cookie = gifUrl;
+      }
+
+      function init() {
+        divs = document.body.getElementsByClassName("gifHolder")
+        for(i in divs) {
+          gifDiv = divs[i]
+          gifUrl = gifDiv.getAttribute('data-gif-url')
+          gifDiv.onclick = Function("setGif(" + gifDiv.id + ")")
+          gifDiv.style.background = "url('" + gifUrl + "')"
+        }
+      }
+      window.onload = Function("init()");
+    </script>
+  </head>
+  <body>
+EOD
+i = 0
+divs = gifs.collect do |gifUrl|
+  i += 1
+  "<div id='#{i}' class='gifHolder' data-gif-url='#{gifUrl}'>&nbsp;</div>"
+end.compact.join("\n")
+footer = <<-EOD
+  <body>
+</html>
+EOD
+File.open("picker.html", "w").write([header,divs,footer].join("\n"))
